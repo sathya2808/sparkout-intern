d83b33041e6c20e6b4394ac42a63b496a1e624ea
sathya <sksathya2810@gmail.com>
2022-01-17 18:02:28 +0530

    newline

--- sameornot/src/sameornot/equal.java
+++ sameornot/src/sameornot/equal.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class equal {
 	public static void main(String[] args) {
 		int a;
+	
 		int b;
 		Scanner in = new Scanner(System.in);  
 	    System.out.print("Enter a value: ");  
